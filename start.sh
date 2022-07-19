92d63f1b22337a4904e30eba7337ef97c5bdc21d
Nathaniel Davidson <nathaniel.davidson@gmail.com>
2022-07-19 15:57:03 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,11 +10,17 @@ fi
 
 mkdir ~/.ssh/
 ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
-
-
 cp /ssh/key ~/.ssh/id_rsa
 chmod 0600 ~/.ssh/id_rsa
+
 git clone $GIT_REPO .
+if [[ -z "${GIT_BRANCH}" ]]; then
+    git checkout $GIT_BRANCH
+fi
+if [[ -z "${GIT_COMMIT}" ]]; then
+    git commit $GIT_COMMIT
+fi
+
 cd $WORKDIR
 
 buildah build -f $DOCKERFILE -t $REGISTRY_HOST/$DOCKER_TAG:$DOCKER_VERSION .
