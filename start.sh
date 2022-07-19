8ccb9ea709901217c33a221cfd7df70771307c67
Nathaniel Davidson <nathaniel.davidson@gmail.com>
2022-07-18 18:17:28 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ if test -f "$FILE"; then
     echo "$FILE exists."
 fi
 
-git clone $GIT_REPO .
+GIT_SSH_COMMAND="ssh -i /ssh/key" git clone $GIT_REPO .
 cd $WORKDIR
 
 buildah build -f $DOCKERFILE -t $DOCKER_TAG:$DOCKER_VERSION .
