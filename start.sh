1ede38b49290b44b85e12aa6217b0c5333cc32a0
Nathaniel Davidson <nathaniel.davidson@gmail.com>
2022-07-18 19:48:21 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,6 +7,10 @@ FILE=/ssh/key
 if test -f "$FILE"; then
     echo "$FILE exists."
 fi
+
+ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+
+
 cp /ssh/key /ssh_tmp_key
 chmod 0600 /ssh_tmp_key
 ssh-agent bash -c 'ssh-add /ssh_tmp_key; git clone $GIT_REPO .'
