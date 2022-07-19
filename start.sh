82cdd2c617a5ff1b740015dd39fbee6db5269a94
Nathaniel Davidson <nathaniel.davidson@gmail.com>
2022-07-18 19:57:09 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -17,4 +17,8 @@ chmod 0600 ~/.ssh/id_rsa
 git clone $GIT_REPO .
 cd $WORKDIR
 
-buildah build -f $DOCKERFILE -t $DOCKER_TAG:$DOCKER_VERSION .
+buildah build -f $DOCKERFILE -t $REGISTRY_HOST/$DOCKER_TAG:$DOCKER_VERSION .
+
+echo $REGISTRY_PASSWORD | buildah login -u $REGISTRY_USERNAME --password-stdin
+
+buildah push $REGISTRY_HOST/$DOCKER_TAG:$DOCKER_VERSION
